91aee1ade05be7c2e851516c9ad45c29dd7f6a00
SerCycle <michelyanuar@gmail.com>
2023-06-30 01:56:43 +0700

    setup layout vue

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="flex items-center justify-center min-h-screen">
+    <div class="w-[360px] h-[800px] bg-gray-400">
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  </div>
+</template>
+
