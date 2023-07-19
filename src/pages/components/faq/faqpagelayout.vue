e15e8ff5b4a05e887958596d5375c195515a6d21
SerCycle <michelyanuar@gmail.com>
2023-07-20 02:08:48 +0700

    add faq page

---
+++ src/pages/components/faq/faqpagelayout.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="flex items-center justify-center min-h-screen">
+        <div class="w-[360px] h-[800px] bg-gray-400">
+            test
+        </div>
+    </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,13 @@ import { createRouter, createWebHistory } from "vue-router";
 // import template
 import TPhone from '../pages/TemplatePhone.vue';
 
-// import home
-import Home from '../pages/components/homepage.vue'
+// import 3 page utama
+import Home from '../pages/components/homepage.vue';
+import Historypeminjaman from '../pages/components/historypage.vue';
+import Account from '../pages/components/accountpage.vue';
+
+// Import FaQPage
+import FaQPage from '../pages/components/faq/faqpagelayout.vue';
 
 const routes = [
     {
@@ -14,15 +19,39 @@ const routes = [
         children:[
             {
                 name: 'Homepage',
-                path: '/',
+                path: '/homepage',
                 component:Home
             },
-        ]
-    }
+            {
+                name: 'History',
+                path: '/historypeminjaman',
+                component:Historypeminjaman
+            },
+            {
+                name: 'Account',
+                path: '/account',
+                component:Account
+            },
+        ],
+    },
+    {
+        name: 'FaQPage',
+        path: '/faqpage',
+        component: FaQPage,
+    },
 ];
 
 const router = createRouter({
     history: createWebHistory(),
     routes
   });
+
+router.beforeEach(async (to) => {
+    if (
+        to.path == '/'
+    ) {
+        return { path: '/homepage' }
+    }
+})
+
 export default router;
