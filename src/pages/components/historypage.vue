8ae6b85c9ae10996177085e4068d01dc92a763e9
SerCycle <michelyanuar@gmail.com>
2023-07-21 02:42:34 +0700

    update history page

--- src/pages/components/historypage.vue
+++ src/pages/components/historypage.vue
@@ -1,5 +1,202 @@
 <template>
-    <div>
-        history
+
+    <!-- Header -->
+    <div class="h-[82px] flex justify-center items-center bg-white shadow sticky top-0">
+        <div class="w-[320px] flex justify-between">
+            <div class="w-[95px]">
+                <p class="text-[22px] leading-[22px] font-bold text-[#365B4F]">Riwayat Pinjaman</p>
+            </div>
+            <div class="flex items-center">
+                <img class="aspect-square w-[31px] h-[31px] rounded-full" src="../../components/icons/headphones.png" alt="">
+            </div>
+        </div>
+    </div>
+
+    <!-- Daftar list history -->
+    <div class="ps-[20px] pt-[14px]">
+        <div class="h-[188px] w-[320px] bg-white shadow-md rounded-[14px]">
+            
+            <!-- Header Card -->
+            <div class="flex justify-between bg-[#DCF7E1] rounded-t-[14px] h-[26px]">
+                <div class="ps-[25px] pt-[5px]">
+                    <p class="text-[#398B62] text-[11px]">No Pesanan: V453YBU6</p>
+                </div>
+                <div class="text-center h-[17px] w-[92px] bg-[#FF5055] rounded-bl-[14px] rounded-tr-[14px]">
+                    <p class="text-white text-[11px]">Lewat 12 hari</p>
+                </div>
+            </div>
+
+            <!-- Card Body -->
+            <div class="flex justify-between">
+                <div class="ps-[22px]">
+                    <div class="pt-[11px]">
+                        <p class="text-[#C6C6C6] text-[14px] leading-[20px]">Jumlah Pinjaman</p>
+                    </div>
+                    <div>
+                        <p class="text-[14px] leading-[20px] font-bold text-[#424242]">Rp. 2,300,000</p>
+                    </div>
+                </div>
+                <div class="pe-[22px]">
+                    <div class="pt-[11px]">
+                        <p class="text-[#C6C6C6] text-[14px] leading-[20px]">Tanggal Pengajuan</p>
+                    </div>
+                    <div>
+                        <p class="text-[14px] leading-[20px] font-bold text-[#424242]">07/03/2023</p>
+                    </div>
+                </div>
+            </div>
+            <div class="flex justify-between">
+                <div class="ps-[22px]">
+                    <div class="pt-[11px]">
+                        <p class="text-[#C6C6C6] text-[14px] leading-[20px]">Jumlah Tagihan</p>
+                    </div>
+                    <div>
+                        <p class="text-[14px] leading-[20px] font-bold text-[#424242]">Rp. 2,393,800</p>
+                    </div>
+                </div>
+                <div class="pe-[22px]">
+                    <div class="pt-[11px]">
+                        <p class="text-[#C6C6C6] text-[14px] leading-[20px]">Batas Pembayaran</p>
+                    </div>
+                    <div class="w-[120.44px]">
+                        <p class="text-[14px] leading-[20px] font-bold text-[#424242]">21/03/2023</p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Footer Card -->
+            <div class="flex justify-between pt-[11px] items-center">
+                <div class="ps-[22px]">
+                    <p class="text-[14px] font-medium leading-[20px] text-[#016F30]">Detail Pinjaman</p>
+                </div>
+                <div class="pe-[22px]">
+                    <div class="bg-[#016F30] w-[89px] h-[38px] flex justify-center items-center rounded-[100px]">
+                        <p class="text-white text-[16px] leading-[24px]">Bayar</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="pt-[14px]">
+            <div class="h-[93px] w-[320px] bg-white shadow-md rounded-[14px]">
+                <div class="flex justify-between bg-[#DCF7E1] rounded-t-[14px] h-[26px]">
+                    <div class="ps-[25px] pt-[5px]">
+                        <p class="text-[#398B62] text-[11px]">No Pesanan: Q2W3E4R5</p>
+                    </div>
+                    <div class="bg-[#92B2A3] text-center w-[110px] h-[17px] rounded-bl-[14px] rounded-tr-[14px]">
+                        <p class="text-[11px] text-white">Sudah Melunasi</p>
+                    </div>
+                </div>
+                <div class="flex justify-between">
+                    <div class="ps-[22px]">
+                        <div class="pt-[11px]">
+                            <p class="text-[#C6C6C6] text-[14px] leading-[20px]">Jumlah Pinjaman</p>
+                        </div>
+                        <div>
+                            <p class="text-[14px] leading-[20px] font-bold text-[#424242]">Rp. 2,300,000</p>
+                        </div>
+                    </div>
+                    <div class="pe-[22px]">
+                        <div class="pt-[11px]">
+                            <p class="text-[#C6C6C6] text-[14px] leading-[20px]">Tanggal Pengajuan</p>
+                        </div>
+                        <div>
+                            <p class="text-[14px] leading-[20px] font-bold text-[#424242]">07/03/2023</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="pt-[14px]">
+            <div class="h-[93px] w-[320px] bg-white shadow-md rounded-[14px]">
+                <div class="flex justify-between bg-[#DCF7E1] rounded-t-[14px] h-[26px]">
+                    <div class="ps-[25px] pt-[5px]">
+                        <p class="text-[#398B62] text-[11px]">No Pesanan: FWEF4FSF</p>
+                    </div>
+                    <div class="bg-[#92B2A3] text-center w-[110px] h-[17px] rounded-bl-[14px] rounded-tr-[14px]">
+                        <p class="text-[11px] text-white">Tinjauan Ditolak</p>
+                    </div>
+                </div>
+                <div class="flex justify-between">
+                    <div class="ps-[22px]">
+                        <div class="pt-[11px]">
+                            <p class="text-[#C6C6C6] text-[14px] leading-[20px]">Jumlah Pinjaman</p>
+                        </div>
+                        <div>
+                            <p class="text-[14px] leading-[20px] font-bold text-[#424242]">Rp. 2,300,000</p>
+                        </div>
+                    </div>
+                    <div class="pe-[22px]">
+                        <div class="pt-[11px]">
+                            <p class="text-[#C6C6C6] text-[14px] leading-[20px]">Tanggal Pengajuan</p>
+                        </div>
+                        <div>
+                            <p class="text-[14px] leading-[20px] font-bold text-[#424242]">04/03/2023</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="pt-[14px]">
+            <div class="h-[93px] w-[320px] bg-white shadow-md rounded-[14px]">
+                <div class="flex justify-between bg-[#DCF7E1] rounded-t-[14px] h-[26px]">
+                    <div class="ps-[25px] pt-[5px]">
+                        <p class="text-[#398B62] text-[11px]">No Pesanan: 3243FSDF</p>
+                    </div>
+                    <div class="bg-[#92B2A3] text-center w-[110px] h-[17px] rounded-bl-[14px] rounded-tr-[14px]">
+                        <p class="text-[11px] text-white">Tinjauan Ditolak</p>
+                    </div>
+                </div>
+                <div class="flex justify-between">
+                    <div class="ps-[22px]">
+                        <div class="pt-[11px]">
+                            <p class="text-[#C6C6C6] text-[14px] leading-[20px]">Jumlah Pinjaman</p>
+                        </div>
+                        <div>
+                            <p class="text-[14px] leading-[20px] font-bold text-[#424242]">Rp. 2,300,000</p>
+                        </div>
+                    </div>
+                    <div class="pe-[22px]">
+                        <div class="pt-[11px]">
+                            <p class="text-[#C6C6C6] text-[14px] leading-[20px]">Tanggal Pengajuan</p>
+                        </div>
+                        <div>
+                            <p class="text-[14px] leading-[20px] font-bold text-[#424242]">01/03/2023</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="pt-[14px] pb-[14px]">
+            <div class="h-[93px] w-[320px] bg-white shadow-md rounded-[14px]">
+                <div class="flex justify-between bg-[#DCF7E1] rounded-t-[14px] h-[26px]">
+                    <div class="ps-[25px] pt-[5px]">
+                        <p class="text-[#398B62] text-[11px]">No Pesanan: 7DXV8DJ9</p>
+                    </div>
+                    <div class="bg-[#92B2A3] text-center w-[110px] h-[17px] rounded-bl-[14px] rounded-tr-[14px]">
+                        <p class="text-[11px] text-white">Sudah Melunasi</p>
+                    </div>
+                </div>
+                <div class="flex justify-between">
+                    <div class="ps-[22px]">
+                        <div class="pt-[11px]">
+                            <p class="text-[#C6C6C6] text-[14px] leading-[20px]">Jumlah Pinjaman</p>
+                        </div>
+                        <div>
+                            <p class="text-[14px] leading-[20px] font-bold text-[#424242]">Rp. 8,500,000</p>
+                        </div>
+                    </div>
+                    <div class="pe-[22px]">
+                        <div class="pt-[11px]">
+                            <p class="text-[#C6C6C6] text-[14px] leading-[20px]">Tanggal Pengajuan</p>
+                        </div>
+                        <div>
+                            <p class="text-[14px] leading-[20px] font-bold text-[#424242]">02/02/2023</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
