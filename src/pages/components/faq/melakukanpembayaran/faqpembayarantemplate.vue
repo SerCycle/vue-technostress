3be18efb40d9ecd6a56c9a6365bb9dc0ce9838b7
SerCycle <michelyanuar@gmail.com>
2023-07-22 01:01:02 +0700

    upload template header inside page

---
+++ src/pages/components/faq/melakukanpembayaran/faqpembayarantemplate.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="flex items-center justify-center min-h-screen">
+        <div class="w-[360px] h-[800px] bg-white rounded-2xl">
+            melakukan pembayaran page
+        </div>
+    </div>
+</template>
