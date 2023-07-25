60b1e6535498b1232aacc9d2c8cf0f3a72ced1f5
Alif Yanuar Aditya <63470202+SerCycle@users.noreply.github.com>
2023-07-25 21:08:06 +0700

    debug layout

--- src/pages/components/faq/faqpagelayout.vue
+++ src/pages/components/faq/faqpagelayout.vue
@@ -43,8 +43,8 @@
                 </router-link>
 
                 <!-- Melakukan Pembayaran -->
-                <router-link to="/faq/pembayaran" class="w-[320px] h-[58px] bg-[#007A3B] flex justify-between items-center rounded-[14px]">
-                    <p class="ps-[20px] text-white">Melakukan Pemabayaran</p>
+                <router-link to="/faq/pembayaran/1" class="w-[320px] h-[58px] bg-[#007A3B] flex justify-between items-center rounded-[14px]">
+                    <p class="ps-[20px] text-white">Melakukan Pembayaran</p>
                     <div class="pe-[20px] h-[53px] flex items-center">
                         <svg class="h-[22px] w-[13px] fill-[#ffffff]" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512">
                             <path d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z"/>
