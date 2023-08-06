32e9e602c72e5579d3425439a00ccf9b50011d8a
SerCycle <michelyanuar@gmail.com>
2023-08-06 20:30:35 +0700

    update simulation

--- src/pages/components/simulation/simulbefore.vue
+++ src/pages/components/simulation/simulbefore.vue
@@ -1,5 +1,43 @@
 <template>
-    <div>
-        before
+    <div class="pt-[17px]">
+        <div class="w-[320px]">
+            <p class="text-[12px] leading-[16px] text-center">Fitur ini akan memberikan sebuah gambaran  terbaik mengenai pinjaman yang akan anda ajukan. Gunakanlah sesuai jumlah yang anda inginkan.</p>
+        </div>
+
+        <form action="">
+            <div class="ps-[20px] pt-[17px]">
+                <div class="pb-[12px]">
+                    <label for="tanggal_peminjaman" class="text-[14px] leading-[20px] font-medium">Tanggal Peminjaman</label>
+                    <input id="tanggal_peminjaman" name="tanggal_peminjaman" type="date" placeholder="05/04/2023" class="h-[34px] w-[320px] shadow appearance-none border py-2 px-7 leading-tight focus:outline-none focus:shadow-outline rounded-[14px]">
+                </div>
+                <div class="pb-[12px]">
+                    <label for="nominal_peminjaman" class="text-[14px] leading-[20px] font-medium">Nominal Peminjaman</label>
+                    <input id="nominal_peminjaman" name="nominal_peminjaman" type="number" placeholder="Masukkan Jumlah Nominal" class="h-[34px] w-[320px] placeholder:text-[#C9C9C9] shadow appearance-none border py-2 px-7 leading-tight focus:outline-none focus:shadow-outline rounded-[14px]">
+                </div>
+                <div class="pb-[12px]">
+                    <label for="durasi_peminjaman" class="text-[14px] leading-[20px] font-medium">Durasi Peminjaman</label>
+                    <select id="durasi_peminjaman" name="durasi_peminjaman" class="h-[34px] w-[320px] shadow appearance-none border py-2 px-7 leading-tight focus:outline-none focus:shadow-outline rounded-[14px] text-[#B9B9B9] focus:text-black">
+                        <option value="volvo">Pilih Durasi</option>
+                        <option value="saab">3 Bulan</option>
+                        <option value="mercedes">6 Bulan</option>
+                        <option value="audi">12 Bulan</option>
+                    </select>
+                </div>
+                <div class="pb-[12px]">
+                    <p class="text-[14px] leading-[20px] font-medium">Suku Bunga (Max)</p>
+                    <p class="text-[14px] leading-[20px] font-bold">19 %</p>
+                </div>
+                <div class="">
+                    <label for="biaya_layanan" class="text-[14px] leading-[20px] font-medium">Biaya Layanan</label>
+                    <p class="text-[14px] leading-[20px] font-bold">0</p>
+                </div>
+            </div>
+
+            <div class="ps-[20px] pt-[238px]">
+                <button class="bg-[#016F30] w-[320px] h-[38px] flex justify-center items-center rounded-[100px]">
+                    <p class="text-white text-[16px] leading-[24px]">Simulasi</p>
+                </button>
+            </div>
+        </form>
     </div>
 </template>
--- src/pages/components/simulation/simulpage.vue
+++ src/pages/components/simulation/simulpage.vue
@@ -1,5 +1,28 @@
 <template>
-    <div>
-        template simul
+    <div class="flex items-center justify-center min-h-screen">
+        <div class="w-[360px] h-[800px] bg-white rounded-2xl">
+
+            <!-- Header -->
+            <div class="h-[80px] flex justify-center items-center rounded-t-2xl bg-white shadow sticky top-0">
+                <div class="w-[360px] flex justify-center">
+                    <router-link to="/homepage" class="h-[80px] w-[80px] flex justify-center items-center">
+                        <svg class="h-[22px] w-[13px] fill-[#000000]" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512">
+                            <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/>
+                        </svg>
+                    </router-link> 
+                    <div class="flex items-center ps-[51px] pe-[73px]">
+                        <p class="text-[22px] leading-[28px] font-bold text-black w-[95px]">Simulasi Pinjaman</p>
+                    </div>
+                    <div class="pe-[30px] flex items-center">
+                        <img class="aspect-square w-[31px] h-[31px] rounded-full" src="../../../components/icons/headphones.png" alt="">
+                    </div>
+                </div>
+            </div>
+
+            <!-- router view -->
+            <div class="w-full flex justify-center">
+                <router-view></router-view>
+            </div>
+        </div>
     </div>
 </template>
